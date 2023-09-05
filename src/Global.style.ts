6e007a63619691fc779a8a4c0067c0cf8b512526
Vinicius Jara <viniciusfjara@gmail.com>
2023-09-05 08:41:46 -0400

    adição de fonte bold e semibold

--- src/Global.style.ts
+++ src/Global.style.ts
@@ -1,11 +1,25 @@
 import {createGlobalStyle} from 'styled-components'
 import PoppinsRegular from './assets/fonts/Poppins-Regular.ttf'
+import PoppinsSemiBold from './assets/fonts/Poppins-SemiBold.ttf'
+import PoppinsBold from './assets/fonts/Poppins-Bold.ttf'
 
 export const GlobalStyle = createGlobalStyle`
     @font-face {
-        font-family: 'PoppinsRegular';
+        font-family: 'Poppins';
         src: url(${PoppinsRegular});
         font-weight: 400;
+        font-style: normal;
+    }
+    @font-face {
+        font-family: 'Poppins';
+        src: url(${PoppinsSemiBold});
+        font-weight: 600;
+        font-style: normal;
+    }
+    @font-face {
+        font-family: 'Poppins';
+        src: url(${PoppinsBold});
+        font-weight: 700;
         font-style: normal;
     }
     *{
@@ -13,7 +27,7 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         text-decoration: none;
-        font-family: PoppinsRegular;
+        font-family: Poppins;
         font-size: 16px;
     }
 `
